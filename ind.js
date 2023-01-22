b3f176c73a466435b6d0183b980d25942e559708
tarun520 <78148275+tarun520@users.noreply.github.com>
2023-01-22 12:50:34 +0530

    Update ind.js

--- ind.js
+++ ind.js
@@ -1,3 +1,8 @@
-
-var header=document.querySelector('#main-header')
-header.style.borderBottom='solid 4px #ccc'
+// var items=document.querySelectorAll('.list-group-item')
+// items[1].style.background='green'
+// items[2].style.color='white'
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.background='green'
+}
