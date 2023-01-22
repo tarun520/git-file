efff2c1f4492de57deabc7bf99fc17b8cab3d252
tarun520 <78148275+tarun520@users.noreply.github.com>
2023-01-22 12:30:48 +0530

    Update ind.js

--- ind.js
+++ ind.js
@@ -1,10 +1,3 @@
- console.log(document.domain);
- console.log(document.URL);
- var items=document.getElementsByClassName('list-group-item');
- console.log(items[1]);
- items[1].textContent='hello';
- items[1].style.fontWeight='bold';
- for(var i=0;i<items.length;i++)
- {
-    items[i].style.backgroundColor='#f4f4f4'
- }
+
+var header=document.querySelector('#main-header')
+header.style.borderBottom='solid 4px #ccc'
