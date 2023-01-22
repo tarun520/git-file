c7fcbbc0b87bd5b55236e4d5e6f8f57f98191b3c
tarun520 <78148275+tarun520@users.noreply.github.com>
2023-01-22 12:17:28 +0530

    get element by class name

--- ind.js
+++ ind.js
@@ -1,3 +1,10 @@
  console.log(document.domain);
  console.log(document.URL);
- 
+ var items=document.getElementsByClassName('list-group-item');
+ console.log(items[1]);
+ items[1].textContent='hello';
+ items[1].style.fontWeight='bold';
+ for(var i=0;i<items.length;i++)
+ {
+    items[i].style.backgroundColor='#f4f4f4'
+ }
